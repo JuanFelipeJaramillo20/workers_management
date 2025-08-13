1b90e051bf9e476cb0fbd3f93608a6574b2d0e5f
Juan Felipe Jaramillo <juanfelipejaramillolosada@gmail.com>
2025-08-13 04:49:52 -0500

    change mysql image version

--- src/test/java/com/tg1/tg1/Employee/EmployeeControllerIntegrationTest.java
+++ src/test/java/com/tg1/tg1/Employee/EmployeeControllerIntegrationTest.java
@@ -23,7 +23,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 public class EmployeeControllerIntegrationTest {
 
-    private static MySQLContainer<?> mysqlContainer = new MySQLContainer<>("mysql:latest");
+    private static MySQLContainer<?> mysqlContainer = new MySQLContainer<>("mysql:9.4");
 
     @LocalServerPort
     private int port;
